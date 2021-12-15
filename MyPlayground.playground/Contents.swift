06c591e0d1b09b3e33a4d99534a68270b97fa430
蒋志鹏 <jiangzhipeng@mgtv.com>
2021-12-15 17:32:33 +0800

    字典和数组的初始化，以及应用

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,1 +1,26 @@
 import UIKit
+
+
+var shoppingList = ["catfish","water","tulips","blue paint"]
+shoppingList[1] = "bottle of water"
+var occupations = [
+    "Malcolm":"Captain",
+    "Kaylee" :"Mechanic"
+]
+
+occupations["Kaylee"] = "Public Relations"
+
+print(shoppingList)
+print(occupations)
+
+shoppingList.append("blue paint")
+print(shoppingList)
+shoppingList.remove(at: 0)
+print(shoppingList)
+//let emptyArray: [String] = []
+//let emptyDictionary: [String: Float] = [:]
+
+var emptyArray:[String] = []//声明一个空数组 var emptyArr2 = []不合法
+var emptyDictionary :[String : Float] = [:]//声明一个空字典
+
+
