c55ab1bbddc1250408e970c362f9a67933c2e80c
蒋志鹏 <jiangzhipeng@mgtv.com>
2021-12-15 17:36:30 +0800

    标题补充

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-
+//数组字典的声明和使用
 var shoppingList = ["catfish","water","tulips","blue paint"]
 shoppingList[1] = "bottle of water"
 var occupations = [
@@ -22,5 +22,7 @@ print(shoppingList)
 
 var emptyArray:[String] = []//声明一个空数组 var emptyArr2 = []不合法
 var emptyDictionary :[String : Float] = [:]//声明一个空字典
+
+
 
 
